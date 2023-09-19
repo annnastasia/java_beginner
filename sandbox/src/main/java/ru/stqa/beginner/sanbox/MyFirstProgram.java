c5dba73b22ed349662478d374d39994ff577b002
Nastia <nastunich@yandex.ru>
2023-09-19 22:13:20 +0300

    создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stqa/beginner/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/beginner/sanbox/MyFirstProgram.java
@@ -1,5 +1,6 @@
-public class MyFirstProgramm {
+package ru.stqa.beginner.sanbox;
 
+public class MyFirstProgram {
 
  public static void main(String[] agrs) {
   System.out.println("Hello, world!");
